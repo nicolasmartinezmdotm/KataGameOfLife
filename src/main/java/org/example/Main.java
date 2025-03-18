f54526f7763f2d925c797a346567476512774086
Nico <n@n.n>
2025-03-18 11:39:41 +0100

    First test updated and passed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,19 @@ public class Main {
         public String GameOfLife() {
             boolean[][] board = new boolean[4][8];
 
+            board[1][4] = true;
+            board[2][3] = true;
+            board[2][4] = true;
+
             String output = "";
 
             for (int row = 0; row < board.length; row++) {
                 for (int col = 0; col < board[row].length; col++) {
-                    output += ".";
+                    if (board[row][col]) {
+                        output += "*";
+                    } else {
+                        output += ".";
+                    }
                 }
                 output += "\n";
             }
