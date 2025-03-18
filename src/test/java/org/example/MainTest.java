5ef23a6277d6a51bd60f2022f150bee89fda1efa
Nico <n@n.n>
2025-03-18 11:32:21 +0100

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class MainTest {
+    Main.GameOfLife gameOfLife = new Main.GameOfLife();;
+
+    @Test
+    void checkIfThereIsABoard() {
+        String result = gameOfLife.Board();
+
+        assertEquals("""
+                ........
+                ........
+                ........
+                ........
+                """, result);
+    }
+}
